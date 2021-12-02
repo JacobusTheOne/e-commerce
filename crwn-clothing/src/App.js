37f78546489604174143e3178ee4fd79bf3e5823
JacobusTheOne <jacobus.jacobs1@yahoo.com>
2021-12-02 08:55:57 +0200

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,13 +1,55 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
-function App() {
+function HatsPage() {
   return (
     <div>
-      <HomePage></HomePage>
+      <h1>HATS PAGE</h1>
     </div>
+  );
+}
+function JacketsPage() {
+  return (
+    <div>
+      <h1>JACKET PAGE</h1>
+    </div>
+  );
+}
+function SneakersPage() {
+  return (
+    <div>
+      <h1>SNEAKERS PAGE</h1>
+    </div>
+  );
+}
+function WomensPage() {
+  return (
+    <div>
+      <h1>WOMENS PAGE</h1>
+    </div>
+  );
+}
+function MensPage() {
+  return (
+    <div>
+      <h1>MENS PAGE</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="" element={<HomePage />} />
+      <Route path="shop/hats" element={<HatsPage />} />
+      <Route path="shop/jackets" element={<JacketsPage />} />
+      <Route path="shop/sneakers" element={<SneakersPage />} />
+      <Route path="shop/womens" element={<WomensPage />} />
+      <Route path="shop/mens" element={<MensPage />} />
+    </Routes>
   );
 }
 
--- crwn-clothing/src/components/menu-item/menu-item.component.js
+++ crwn-clothing/src/components/menu-item/menu-item.component.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./menu-item.styles.scss";
-export default function MenuItem({ title, imageUrl, size }) {
+import { useNavigate } from "react-router-dom";
+export default function MenuItem({ title, imageUrl, size, linkUrl }) {
+  const navigate = useNavigate();
   return (
     <div className={`${size} menu-item`}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
-      <div className="content">
+      <button className="content" onClick={() => navigate(linkUrl)}>
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
-      </div>
+      </button>
     </div>
   );
 }
