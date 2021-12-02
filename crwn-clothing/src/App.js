a83da7ba2a490131eade10c9fe07c51ab241e244
JacobusTheOne <jacobus.jacobs1@yahoo.com>
2021-12-02 10:53:59 +0200

    Adding header component and links to home and the shop page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,50 +2,18 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "../src/components/header/header.component.jsx";
 import "./App.css";
-
-function HatsPage() {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-}
-function JacketsPage() {
-  return (
-    <div>
-      <h1>JACKET PAGE</h1>
-    </div>
-  );
-}
-function SneakersPage() {
-  return (
-    <div>
-      <h1>SNEAKERS PAGE</h1>
-    </div>
-  );
-}
-function WomensPage() {
-  return (
-    <div>
-      <h1>WOMENS PAGE</h1>
-    </div>
-  );
-}
-function MensPage() {
-  return (
-    <div>
-      <h1>MENS PAGE</h1>
-    </div>
-  );
-}
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<HomePage />} />
-      <Route path="/shop" element={<ShopPage />} />
-    </Routes>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </div>
   );
 }
 
